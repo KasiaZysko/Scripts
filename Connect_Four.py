c58710c830ae74f0c8c684aaa7703781a58dac8c
Katarzyna Zysko <45132764+Kacha107@users.noreply.github.com>
2020-01-29 11:21:43 +0100

    Update Connect_Four.py

--- Connect_Four.py
+++ Connect_Four.py
@@ -1,3 +1,9 @@
+""" Kata from codewars.com 
+The grid is 6 row by 7 columns, those being named from A to G.
+You will receive a list of strings showing the order of the pieces which dropped in columns:
+The list may contain up to 42 moves and shows the order the players are playing.
+The first player who connects four items of the same color is the winner.
+You should return "Yellow", "Red" or "Draw" accordingly. """
 import numpy as np
 
 row_count = 6
